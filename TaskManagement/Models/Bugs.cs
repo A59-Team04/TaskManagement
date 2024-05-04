bcd96f4154491e7bc8a4b7c954e1b6c026c37470
Alexander Georgiev <alexander.nikolaev.georgiev@gmail.com>
2024-05-04 16:49:59 +0300

    Additional models

---
+++ TaskManagement/Models/Bugs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Models
+{
+    internal class Bugs
+    {
+        public int titleMinLenght = 10;
+        public int titleMaxLenght = 50;
+        public int descriptionMinLenght = 10;
+        public int descriptionMaxLenght = 500;
+
+    }
+}
---
+++ TaskManagement/Models/Task.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Models.Contracts;
+
+namespace TaskManagement.Models
+{
+    internal class Task : ITask
+    {
+        public static int titleMinLenght = 10;
+        public static int titleMaxLenght = 50;
+        public static string titcleLenghtError = $"The title must be between {titleMinLenght} and {titleMaxLenght} characters long";
+        public static int descriptionMinLenght = 10;
+        public static int descriptionMaxLenght = 500;
+        public static string descriptionLenghtError = $"The descripton must be between {descriptionMinLenght} and {descriptionMaxLenght}characters long";
+
+        private string _title;
+        private string _description;
+
+        public Task(string title, string description, TaskType type)
+        {
+            _title = title;
+            _description = description;
+        }
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            private set
+            {
+                Validator.ValidateIntRange(value.Length, titleMinLenght, titleMaxLenght, titcleLenghtError);
+            }
+        }
+
+        public string Description => throw new NotImplementedException();
+
+        public List<Bug> Bugs => throw new NotImplementedException();
+
+        public List<Story> Stories => throw new NotImplementedException();
+
+        public List<Feedback> Feedbacks => throw new NotImplementedException();
+    }
+}
